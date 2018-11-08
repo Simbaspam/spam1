4fea4cd715871ff1843803962b6c0ec16ae9ecf5
Simbaspam <44881163+Simbaspam@users.noreply.github.com>
2018-11-08 22:31:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "330122628964810773"; // ايدي السررفر
+var channel = "433722727145144338";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,Simba سبحان الله ,')
     },305);
 })
 
